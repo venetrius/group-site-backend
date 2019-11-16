b15b023157e92c267940e94628e6b1485357729a
venetrius <gergelygjuhasz@gmail.com>
2019-11-16 12:04:20 -0700

    fix migration

--- migrations/20191110162534_init.js
+++ migrations/20191110162534_init.js
@@ -1,24 +1,19 @@
-
 exports.up = function (knex, Promise) {
-  return Promise.all([
-      knex.schema.createTableIfNotExists("projects", function (table) {
-        table.increments('id')
-        table.string('name')
-        table.string('description')
-      }).then(function () {
-              return knex("payment_paypal_status").insert([
-                  {name: "Our Website", description: "A"},
-                  {name: "Scenic routes", description: "BB"},
-                  {name: "AirBnB smart pricing", description: "CCC"},
-                  {name: "Scraping App", description: "DDDD"}
-              ]);
-          }
-      ),
-  ]);
+  return knex.schema.createTable("projects", function (table) {
+    table.increments('id')
+    table.string('name')
+    table.string('description')
+  }).then(function () {
+          return knex("projects").insert([
+            {name: "Our Website", description: "A"},
+            {name: "Scenic routes", description: "BB"},
+            {name: "AirBnB smart pricing", description: "CCC"},
+            {name: "Scraping App", description: "DDDD"}
+          ]);
+      }
+  );
 };
 
 exports.down = function (knex, Promise) {
-  return Promise.all([
-      knex.schema.dropTableIfExists("projects")
-  ]);
+return knex.schema.dropTable("projects")
 };
