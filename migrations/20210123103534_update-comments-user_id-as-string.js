28db4dd8889f98c5aaff1960df2336364503174e
venetrius <gergelygjuhasz@gmail.com>
2021-05-29 11:11:07 -0600

    Work around

--- migrations/20210123103534_update-comments-user_id-as-string.js
+++ migrations/20210123103534_update-comments-user_id-as-string.js
@@ -1,12 +1,12 @@
 
 exports.up = async function(knex, Promise) {
-  await knex.schema.alterTable('comments', function(table) {
-      table.integer('user_id').alter();
-  });
+  // await knex.schema.alterTable('comments', function(table) {
+  //     table.integer('user_id').alter();
+  // });
 }
 
 exports.down = async function(knex, Promise) {
-  await knex.schema.alterTable('comments', function(table) {
-    table.string('user_id').alter();
-  });
+  // await knex.schema.alterTable('comments', function(table) {
+  //   table.string('user_id').alter();
+  // });
 }
--- migrations/20210128194034_update_cols_to_text.js
+++ migrations/20210128194034_update_cols_to_text.js
@@ -1,12 +1,12 @@
 exports.up = async function(knex, Promise) {
-  await knex.schema.alterTable('projects', function(table) {
-      table.string('description').alter();
-      table.string('comments').alter();
-  });
+  // await knex.schema.alterTable('projects', function(table) {
+  //     table.string('description').alter();
+  //     table.string('comments').alter();
+  // });
 }
 
 exports.down = async function(knex, Promise) {
-  await knex.schema.alterTable('comments', function(table) {
-    table.string('user_id').alter();
-  });
+  // await knex.schema.alterTable('comments', function(table) {
+  //   table.string('user_id').alter();
+  // });
 }
